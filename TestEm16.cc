501ef3e11184e03a78a6b2af2cd672a154825396
AnnamariaMazzone <Annamaria.mazzone@ic.cnr.it>
2022-02-23 13:23:49 +0100

    file dimenticato

--- TestEm16.cc
+++ TestEm16.cc
@@ -44,59 +44,96 @@
 #include "G4UIExecutive.hh"
 #include "G4VisExecutive.hh"
 
+#include "G4SystemOfUnits.hh"
+//#include "HistoManager.hh"
+#include "G4AnalysisManager.hh"
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
+extern int fGammaEmessi;
 int main(int argc,char** argv) {
-
-  //detect interactive mode (if no arguments) and define UI session
-  G4UIExecutive* ui = nullptr;
-  if (argc == 1) ui = new G4UIExecutive(argc,argv);
-
-  //Use SteppingVerbose with Unit
-  G4int precision = 4;
-  G4SteppingVerbose::UseBestUnit(precision);
-  
-  //Creating run manager
-  auto runManager = G4RunManagerFactory::CreateRunManager();
     
-  if (argc==3) { 
-     G4int nThreads = G4UIcommand::ConvertToInt(argv[2]);
-     runManager->SetNumberOfThreads(nThreads);
-  }
-
-  //set mandatory initialization classes
-  DetectorConstruction* det;
-  runManager->SetUserInitialization(det = new DetectorConstruction);
-  runManager->SetUserInitialization(new PhysicsList);
-
-  //set user action classes
-  runManager->SetUserInitialization(new ActionInitialization(det));
-
-  //initialize visualization
-  G4VisManager* visManager = nullptr;
-
-  //get the pointer to the User Interface manager
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    //detect interactive mode (if no arguments) and define UI session
+    G4UIExecutive* ui = nullptr;
+    if (argc == 1) ui = new G4UIExecutive(argc,argv);
+    
+    //Use SteppingVerbose with Unit
+    G4int precision = 4;
+    G4SteppingVerbose::UseBestUnit(precision);
+    
+    //Creating run manager
+    auto runManager = G4RunManagerFactory::CreateRunManager();
+    
+    if (argc==3) {
+        G4int nThreads = G4UIcommand::ConvertToInt(argv[2]);
+        runManager->SetNumberOfThreads(nThreads);
+    }
+    
+    //set mandatory initialization classes
+    DetectorConstruction* det;
+    runManager->SetUserInitialization(det = new DetectorConstruction);
+    runManager->SetUserInitialization(new PhysicsList);
+    runManager->SetUserInitialization(new ActionInitialization(det));
+    
+    //initialize visualization
+    G4VisManager* visManager = nullptr;
+    
+    //get the pointer to the User Interface manager
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
     UImanager->ApplyCommand("/control/macroPath /Users/annamaria/lavoro/xray_shielding/TestEm16");
-  if (ui)  {
-    //interactive mode
-    visManager = new G4VisExecutive;
-    visManager->Initialize();
-      UImanager->ApplyCommand("/control/execute run01.mac");
-    // define icons before SessionStart
-//    if (ui->IsGUI()) { UImanager->ApplyCommand("/control/execute gui.mac"); }
-    ui->SessionStart();
-    delete ui;
-  } else  {
-    //batch mode
-    G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
-  }
-
-  //job termination
-  delete visManager;
-  delete runManager;
+    UImanager->ApplyCommand("/control/execute run01.mac");
+    // start a run
+    int numberOfEvent = 1000000;
+    
+    //    G4double lim_inf_S=0.*mm;
+    G4double lim_sup_S=50*mm;
+    G4double fSpessore=20*mm;
+    G4double step_S= 5*mm; //(lim_sup_S-lim_inf_S)/20.;
+    
+    det->SetTargetSize(fSpessore);
+    runManager->ReinitializeGeometry();
+    
+    G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+    analysisManager->SetDefaultFileType("root");
+    analysisManager->SetFileName("PS50_W45_CNT5_100keV");
+    analysisManager->OpenFile();
+    analysisManager->CreateH2("spessore","PS50_W45_CNT5_100keV", 100, 0*mm, 60*mm, 100, 0., 1);
+    
+    while (fSpessore<=lim_sup_S) {
+        runManager->SetUserInitialization(new ActionInitialization(det));
+        runManager->Initialize();
+        runManager->BeamOn(numberOfEvent);
+        G4double yvalue=(G4double)fGammaEmessi/numberOfEvent;
+        analysisManager->FillH2(0,fSpessore,yvalue);
+        fSpessore=fSpessore+step_S;
+        det->SetTargetSize(fSpessore);
+        runManager->ReinitializeGeometry();
+    }
+    //save histograms
+    
+  //  if ( analysisManager->IsActive() ) {
+      analysisManager->Write();
+      analysisManager->CloseFile();
+  //  }
+     
+    if (ui)  {
+        //interactive mode
+        visManager = new G4VisExecutive;
+        visManager->Initialize();
+        
+        // define icons before SessionStart
+        //    if (ui->IsGUI()) { UImanager->ApplyCommand("/control/execute gui.mac"); }
+        ui->SessionStart();
+        delete ui;
+    } else  {
+        //batch mode
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+    }
+    
+    //job termination
+    delete visManager;
+    delete runManager;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
